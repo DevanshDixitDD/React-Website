cae81b95bdb73b1d4fcd108b69385dbaefa69e5e
Devansh Dixit <102344965+DevanshDixitDD@users.noreply.github.com>
2023-06-20 19:19:45 +0530

    Add files via upload

---
+++ src/pages/FrameComponent.tsx
@@ -0,0 +1,80 @@
+import { FunctionComponent } from "react";
+import styles from "./FrameComponent.module.css";
+const FrameComponent: FunctionComponent = () => {
+  return (
+    <div className={styles.ifqi07sh2x1Parent}>
+      <img
+        className={styles.ifqi07sh2x1Icon}
+        alt=""
+        src="/ifqi07sh-2x-1@2x.png"
+      />
+      <div className={styles.frameChild} />
+      <div className={styles.itHasCapristo}>
+        It has Capristo Carbon Fiber Engine bonnet
+      </div>
+      <div className={styles.theTireUsed}>
+        The tire used in this car is Pirelli tyres
+      </div>
+      <div className={styles.frameItem} />
+      <div className={styles.frameInner} />
+      <div className={styles.ellipseDiv} />
+      <div className={styles.ferrari}>Ferrari</div>
+      <div className={styles.ferrariSpaIsContainer}>
+        <p
+          className={styles.ferrariSpaIs}
+        >{`Ferrari S.p.A. is an Italian luxury `}</p>
+        <p className={styles.ferrariSpaIs}>sports car manufacturer based</p>
+        <p className={styles.ferrariSpaIs}>in Maranello, Italy.</p>
+      </div>
+      <div className={styles.foundedIn1939Container}>
+        <p
+          className={styles.ferrariSpaIs}
+        >{`Founded in 1939 by Enzo Ferrari, `}</p>
+        <p
+          className={styles.ferrariSpaIs}
+        >{`the company adopted its current `}</p>
+        <p
+          className={styles.ferrariSpaIs}
+        >{`name in 1945 and began producing `}</p>
+        <p className={styles.ferrariSpaIs}>its line of cars in 1947.</p>
+      </div>
+      <div className={styles.frameChild1} />
+      <div className={styles.frameChild2} />
+      <div className={styles.frameChild3} />
+      <img className={styles.lineIcon} alt="" src="/line-1.svg" />
+      <img className={styles.frameChild4} alt="" src="/line-2.svg" />
+      <img className={styles.frameChild5} alt="" src="/line-3.svg" />
+      <div className={styles.rectangleDiv} />
+      <div className={styles.frameChild6} />
+      <div className={styles.frameChild7} />
+      <div className={styles.frameChild8} />
+      <div className={styles.startNow}>Start Now</div>
+      <img className={styles.arrowIcon} alt="" src="/arrow-1.svg" />
+      <div className={styles.lineDiv} />
+      <div className={styles.carworld}>CarWorld</div>
+      <img className={styles.carboncarIcon} alt="" src="/carboncar.svg" />
+      <div className={styles.theColorOf}>
+        The Color of the car is Rosso corsa
+      </div>
+      <div className={styles.products}>Products</div>
+      <div className={styles.explore}>Explore</div>
+      <img className={styles.frameChild9} alt="" src="/arrow-4.svg" />
+      <div className={styles.contact}>Contact</div>
+      <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+      <div className={styles.frameChild10} />
+      <div className={styles.frameChild11} />
+      <div className={styles.frameChild12} />
+      <img className={styles.mdiinstagramIcon} alt="" src="/mdiinstagram.svg" />
+      <img className={styles.vectorIcon1} alt="" src="/vector1.svg" />
+      <img className={styles.vectorIcon2} alt="" src="/vector2.svg" />
+      <div className={styles.div}>04 / 09</div>
+      <div className={styles.frameChild13} />
+      <div className={styles.frameChild14} />
+      <div className={styles.frameChild15} />
+      <div className={styles.frameChild16} />
+      <div className={styles.frameChild17} />
+    </div>
+  );
+};
+
+export default FrameComponent;
